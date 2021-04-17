b10a2bd2b7ec9fa0d251c6da91b1c6a3c2d3f2c0
slash6426 <slashnbox2140@gmail.com>
2021-04-17 13:39:02 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,6 @@
-require_relative 'boot'
-
-require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
+# 省略
 module ChatApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
    # 中略
     config.generators do |g|
