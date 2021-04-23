1498889b90ab98e9468963801c1acf801f2cf8d2
slash6426 <slashnbox2140@gmail.com>
2021-04-23 18:17:42 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
-    config.load_defaults 6.0
+    # Initialize configuration defaults for orifginally generated Rails version.
+    config.load_defaults 6.0 
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
 
